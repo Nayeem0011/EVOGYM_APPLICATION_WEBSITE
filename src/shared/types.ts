d14404dcbbeff653b3b39d5f124e344ff69181da
Sk Nayeem <sknayeemislam384@gmail.com>
2025-08-22 20:54:49 +0600

    COMPLETED_EVOGYM_APPLICATION_WEBSITE

---
+++ src/shared/types.ts
@@ -0,0 +1,24 @@
+import type { JSX } from "react";
+
+export const SelectedPage = {
+  Home: "home",
+  Benefits: "benefits",
+  OurClasses: "ourclasses",
+  ContactUs: "contactus",
+  SignUp: "signup",
+} as const;
+
+export type SelectedPage = typeof SelectedPage[keyof typeof SelectedPage];
+
+export interface BenefitType {
+  icon: JSX.Element;
+  title: string;
+  description: string;
+}
+
+export interface ClassType {
+  name: string;
+  description?: string;
+  image: string;
+}
+
